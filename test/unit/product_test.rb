2e30260aca9d14782ecb63b308576244b56520ea
wedzik <wedzikmail@gmail.com>
2015-01-12 00:46:47 +0200

    First module test

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
   #   assert true
-  # end
+  end
 end
